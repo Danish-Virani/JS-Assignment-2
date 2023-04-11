ee245791dbc678292cc03b9d4e42793ecec62e60
Danish-Virani <danishvirani57@gmail.com>
2023-04-11 23:48:09 +0500

    Assignment-2

--- Q4.js
+++ Q4.js
@@ -1,16 +1,13 @@
 function addNewListItem(text) {
-    // create a new list item element
-    const newListItem = document.createElement("li");
-  
-    // set the text content of the new list item
-    newListItem.textContent = text;
-  
-    // get the unordered list element
-    const unorderedList = document.querySelector("ul");
-    console.log(unorderedList);
-    // append the new list item to the unordered list
-    // unorderedList.appendChild(newListItem);
-  }
+  const ul_1 = document.createElement("ul");
+  ul_1.textContent = "This is a name list";
 
-  //Main
-  addNewListItem("Example Item");
+  document.body.appendChild(ul_1);
+  const li = document.createElement("li");
+  li.textContent = text;
+
+  ul_1.appendChild(li);
+}
+
+//Main
+addNewListItem("Danish");
